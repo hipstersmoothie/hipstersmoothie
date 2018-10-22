153ccc80d91224636087b28093d32378920383bc
Andrew Lisowski <andrew_lisowski@intuit.com>
2018-10-22 13:01:09 -0700

    factor out blog into module

--- next.config.js
+++ next.config.js
@@ -2,14 +2,8 @@ const withPlugins = require('next-compose-plugins');
 
 const withCSS = require('@zeit/next-css');
 const withMDX = require('@zeit/next-mdx')();
-const withBlog = require('./next-blog')();
+const withBlog = require('next-blog').Plugin();
 
 module.exports = withPlugins([withCSS, withMDX, withBlog], {
-  pageExtensions: ['js', 'mdx'],
-  exportPathMap: defaultPathMap => {
-    // necessary?
-    const pathMap = Object.assign({}, defaultPathMap);
-    delete pathMap['/index'];
-    return pathMap;
-  }
+  pageExtensions: ['js', 'mdx']
 });
