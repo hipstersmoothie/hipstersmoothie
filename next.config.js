b24fd51f6c5dd06231631be36022a837e694c3b3
Andrew Lisowski <andrew_lisowski@intuit.com>
2018-10-23 20:00:51 -0700

    try 2

--- next.config.js
+++ next.config.js
@@ -3,10 +3,11 @@ const withPlugins = require('next-compose-plugins');
 const withCSS = require('@zeit/next-css');
 const withMDX = require('@zeit/next-mdx')();
 const debug = process.env.NODE_ENV !== 'production';
-const assetPrefix = debug ? '' : '/pages/WHAT/what';
+const assetPrefix = debug ? '' : '/hipstersmoothie/';
 
 // Generates Blog Index
 const withBlog = require('next-mdx-blog').Plugin({
+  assetPrefix,
   author: 'Andrew Lisowski',
   authorLink: 'https://github.intuit.com/alisowski',
   avatar: 'https://avatars2.githubusercontent.com/u/1192452?s=400&v=4'
--- scripts/release-website.sh
+++
@@ -1,5 +0,0 @@
-# rm -rf node_modules/.cache
-next build
-next export
-touch out/.nojekyll
-push-dir --dir=out --branch=gh-pages --cleanup --verbose
